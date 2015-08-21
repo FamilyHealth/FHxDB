3d2e9756643f502387fed642fdd1b36f280d9ddd
Faraz <frzk410@gmail.com>
2015-08-21 13:08:28 +0500

    added database structure image file fhxdb.sql

--- fhxdb.sql
+++ fhxdb.sql
@@ -2,9 +2,9 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.3.6
--- Dumped by pg_dump version 9.3.6
--- Started on 2015-08-21 12:47:31 PKT
+-- Dumped from database version 9.3.9
+-- Dumped by pg_dump version 9.3.9
+-- Started on 2015-08-21 13:04:47
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -14,7 +14,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- TOC entry 180 (class 3079 OID 11789)
+-- TOC entry 175 (class 3079 OID 11750)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -22,8 +22,8 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2019 (class 0 OID 0)
--- Dependencies: 180
+-- TOC entry 1961 (class 0 OID 0)
+-- Dependencies: 175
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -37,7 +37,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 170 (class 1259 OID 16397)
+-- TOC entry 170 (class 1259 OID 16440)
 -- Name: clinical_observation; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -55,31 +55,7 @@ CREATE TABLE clinical_observation (
 ALTER TABLE public.clinical_observation OWNER TO postgres;
 
 --
--- TOC entry 171 (class 1259 OID 16403)
--- Name: clinical_observation_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE clinical_observation_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.clinical_observation_id_seq OWNER TO postgres;
-
---
--- TOC entry 2020 (class 0 OID 0)
--- Dependencies: 171
--- Name: clinical_observation_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE clinical_observation_id_seq OWNED BY clinical_observation.id;
-
-
---
--- TOC entry 172 (class 1259 OID 16405)
+-- TOC entry 171 (class 1259 OID 16446)
 -- Name: disease; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -93,31 +69,7 @@ CREATE TABLE disease (
 ALTER TABLE public.disease OWNER TO postgres;
 
 --
--- TOC entry 173 (class 1259 OID 16411)
--- Name: disease_disease_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE disease_disease_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.disease_disease_id_seq OWNER TO postgres;
-
---
--- TOC entry 2021 (class 0 OID 0)
--- Dependencies: 173
--- Name: disease_disease_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE disease_disease_id_seq OWNED BY disease.disease_id;
-
-
---
--- TOC entry 174 (class 1259 OID 16413)
+-- TOC entry 172 (class 1259 OID 16452)
 -- Name: family_members; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -130,41 +82,17 @@ CREATE TABLE family_members (
     race text,
     weight numeric,
     height numeric,
-    active boolean,
-    created_at timestamp with time zone,
     adopted boolean,
-    twin integer
+    twin integer,
+    active boolean,
+    created_at timestamp with time zone
 );
 
 
 ALTER TABLE public.family_members OWNER TO postgres;
 
 --
--- TOC entry 175 (class 1259 OID 16419)
--- Name: family_members_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE family_members_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.family_members_id_seq OWNER TO postgres;
-
---
--- TOC entry 2022 (class 0 OID 0)
--- Dependencies: 175
--- Name: family_members_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE family_members_id_seq OWNED BY family_members.id;
-
-
---
--- TOC entry 176 (class 1259 OID 16421)
+-- TOC entry 173 (class 1259 OID 16458)
 -- Name: relationships; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -178,31 +106,7 @@ CREATE TABLE relationships (
 ALTER TABLE public.relationships OWNER TO postgres;
 
 --
--- TOC entry 177 (class 1259 OID 16427)
--- Name: relationships_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE relationships_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.relationships_id_seq OWNER TO postgres;
-
---
--- TOC entry 2023 (class 0 OID 0)
--- Dependencies: 177
--- Name: relationships_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE relationships_id_seq OWNED BY relationships.id;
-
-
---
--- TOC entry 178 (class 1259 OID 16429)
+-- TOC entry 174 (class 1259 OID 16464)
 -- Name: relative; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -217,71 +121,7 @@ CREATE TABLE relative (
 ALTER TABLE public.relative OWNER TO postgres;
 
 --
--- TOC entry 179 (class 1259 OID 16435)
--- Name: relative_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE relative_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.relative_id_seq OWNER TO postgres;
-
---
--- TOC entry 2024 (class 0 OID 0)
--- Dependencies: 179
--- Name: relative_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE relative_id_seq OWNED BY relative.id;
-
-
---
--- TOC entry 1890 (class 2604 OID 16437)
--- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY clinical_observation ALTER COLUMN id SET DEFAULT nextval('clinical_observation_id_seq'::regclass);
-
-
---
--- TOC entry 1891 (class 2604 OID 16438)
--- Name: disease_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY disease ALTER COLUMN disease_id SET DEFAULT nextval('disease_disease_id_seq'::regclass);
-
-
---
--- TOC entry 1892 (class 2604 OID 16439)
--- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY family_members ALTER COLUMN id SET DEFAULT nextval('family_members_id_seq'::regclass);
-
-
---
--- TOC entry 1893 (class 2604 OID 16440)
--- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY relationships ALTER COLUMN id SET DEFAULT nextval('relationships_id_seq'::regclass);
-
-
---
--- TOC entry 1894 (class 2604 OID 16441)
--- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY relative ALTER COLUMN id SET DEFAULT nextval('relative_id_seq'::regclass);
-
-
---
--- TOC entry 2002 (class 0 OID 16397)
+-- TOC entry 1949 (class 0 OID 16440)
 -- Dependencies: 170
 -- Data for Name: clinical_observation; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -291,17 +131,8 @@ COPY clinical_observation (id, person_id, disease_id, age_low, age_high, unit, i
 
 
 --
--- TOC entry 2025 (class 0 OID 0)
+-- TOC entry 1950 (class 0 OID 16446)
 -- Dependencies: 171
--- Name: clinical_observation_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('clinical_observation_id_seq', 1, false);
-
-
---
--- TOC entry 2004 (class 0 OID 16405)
--- Dependencies: 172
 -- Data for Name: disease; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -310,36 +141,18 @@ COPY disease (disease_id, display_name, hl7_code) FROM stdin;
 
 
 --
--- TOC entry 2026 (class 0 OID 0)
--- Dependencies: 173
--- Name: disease_disease_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('disease_disease_id_seq', 1, false);
-
-
---
--- TOC entry 2006 (class 0 OID 16413)
--- Dependencies: 174
+-- TOC entry 1951 (class 0 OID 16452)
+-- Dependencies: 172
 -- Data for Name: family_members; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY family_members (id, name, dob, administrative_gender_code, ethnic_group, race, weight, height, active, created_at, adopted, twin) FROM stdin;
+COPY family_members (id, name, dob, administrative_gender_code, ethnic_group, race, weight, height, adopted, twin, active, created_at) FROM stdin;
 \.
 
 
 --
--- TOC entry 2027 (class 0 OID 0)
--- Dependencies: 175
--- Name: family_members_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('family_members_id_seq', 1, false);
-
-
---
--- TOC entry 2008 (class 0 OID 16421)
--- Dependencies: 176
+-- TOC entry 1952 (class 0 OID 16458)
+-- Dependencies: 173
 -- Data for Name: relationships; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -348,17 +161,8 @@ COPY relationships (id, display_name, hl7_code) FROM stdin;
 
 
 --
--- TOC entry 2028 (class 0 OID 0)
--- Dependencies: 177
--- Name: relationships_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('relationships_id_seq', 1, false);
-
-
---
--- TOC entry 2010 (class 0 OID 16429)
--- Dependencies: 178
+-- TOC entry 1953 (class 0 OID 16464)
+-- Dependencies: 174
 -- Data for Name: relative; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -367,17 +171,8 @@ COPY relative (id, person_id, relationship_id, related_to) FROM stdin;
 
 
 --
--- TOC entry 2029 (class 0 OID 0)
--- Dependencies: 179
--- Name: relative_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('relative_id_seq', 1, false);
-
-
---
--- TOC entry 2018 (class 0 OID 0)
--- Dependencies: 6
+-- TOC entry 1960 (class 0 OID 0)
+-- Dependencies: 5
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
 
@@ -387,7 +182,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2015-08-21 12:47:32 PKT
+-- Completed on 2015-08-21 13:04:48
 
 --
 -- PostgreSQL database dump complete
